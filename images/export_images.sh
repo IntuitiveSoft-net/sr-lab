0363df198712b721ce157ef4eafbf5af0f6b4085
Guillaume Ladhuie <gladhuie@gmail.com>
2023-01-05 18:45:51 +0100

    update to svg, update telemetry

--- images/export_images.sh
+++ images/export_images.sh
@@ -2,5 +2,5 @@
 read -p "Export the diagram until : " i
 i=$(($i-1))
 for n in $(seq 0 $i); do
-    /Applications/draw.io.app/Contents/MacOS/draw.io -x -f png -p $n lab-topologies.drawio -o $(($n+1)).png;
+    /Applications/draw.io.app/Contents/MacOS/draw.io -x -f svg -p $n lab-topologies.drawio -o $(($n+1)).svg;
 done
